a19ebeba73ec1a3e39a859308fcfc3d9e272cf79
Kedma Costa <kedmyknow@gmail.com>
2021-06-24 02:55:11 -0300

    update class2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,27 @@
+import { useHistory} from 'react-router-dom'
+import {auth, firebase }from '../services/firebase'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import "../styles/auth.scss"
+import { Button } from '../components/Button'
+
+
 export function Home() {
+  const history = useHistory();
+  
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result=> {
+      console.log(result);
+      
+      history.push('/rooms/new')
+    })
+
+  }
   return (
     <div id="page-auth">
       <aside>
@@ -11,25 +30,25 @@ export function Home() {
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-        </div>
-        <button>
-          <img src={googleIconImg} alt="Logo Google" />
-          Crie sua sala com o Google
-        </button>
-
-        <div>ou entre em uma sala</div>
-
-        <form>
-          <input 
-            type="text"
-            placeholder="Digite o código da sala"
-          />
-          <button type="submit">
-            Entrar na sala
+          <button onClick={handleCreateRoom} className="create-room" >
+            <img src={googleIconImg} alt="Logo Google" />
+            Crie sua sala com o Google
           </button>
-        </form>
+
+          <div className="separator">ou entre em uma sala</div>
+
+          <form>
+            <input 
+              type="text"
+              placeholder="Digite o código da sala"
+              />
+            <Button type="submit">
+              Entrar na sala
+            </Button>
+          </form>
+        </div>
       </main>
 
     </div> 
